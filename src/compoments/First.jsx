44f7899ed08c3d1d0ee0ca1e25467fca8afb73ab
Bar maizel <barmaizel1@gmail.com>
2022-12-13 11:49:18 +0200

    about update

--- src/compoments/First.jsx
+++ src/compoments/First.jsx
@@ -5,18 +5,18 @@ import Row from 'react-bootstrap/Row';
 export default function First(props) {
   return (
     <Row className='first'>
-        <p>
-    Dice art, also known as polyhedral art, is a type of sculpture or craft that involves using dice as the primary medium. These tiny cubes, typically made of plastic or other durable materials, are often used in games and other recreational activities, but they can also be repurposed and arranged in creative ways to create unique and striking visual designs.
+        <p className='aboutText'>
+    Dice art, also known as polyhedral art, is a type of sculpture or craft that involves using dice as the primary medium. These tiny cubes, typically made of plastic or other durable materials, are often used in games and other recreational activities, but they can also be repurposed and arranged in creative ways to create unique and striking visual designs.<br/><br/>
 
-One of the key benefits of using dice for art is that they are highly versatile and can be manipulated in a variety of ways. By carefully arranging the dots on the faces of the dice, artists can create intricate patterns, shapes, and images that would be difficult or impossible to achieve with other materials. This makes dice art a popular choice for those who enjoy working with their hands and experimenting with different design possibilities.
+One of the key benefits of using dice for art is that they are highly versatile and can be manipulated in a variety of ways. By carefully arranging the dots on the faces of the dice, artists can create intricate patterns, shapes, and images that would be difficult or impossible to achieve with other materials. This makes dice art a popular choice for those who enjoy working with their hands and experimenting with different design possibilities.<br/><br/>
 
-To create a dice art piece, the artist begins by choosing a subject or concept that they would like to represent. This could be anything from a simple geometric shape to a complex landscape or portrait. Once they have a clear idea of what they want to create, the artist begins to carefully arrange the dice on a flat surface, paying attention to the placement of the dots on each face.
+To create a dice art piece, the artist begins by choosing a subject or concept that they would like to represent. This could be anything from a simple geometric shape to a complex landscape or portrait. Once they have a clear idea of what they want to create, the artist begins to carefully arrange the dice on a flat surface, paying attention to the placement of the dots on each face.<br/><br/>
 
-As the dice are arranged, the artist may use glue or other adhesives to hold them in place, or they may simply rely on the weight of the dice and their own careful placement to keep them secure. Once the dice are in place, the artist may add additional details or accents using markers, paint, or other materials to further enhance the design.
+As the dice are arranged, the artist may use glue or other adhesives to hold them in place, or they may simply rely on the weight of the dice and their own careful placement to keep them secure. Once the dice are in place, the artist may add additional details or accents using markers, paint, or other materials to further enhance the design.<br/><br/>
 
-One of the most challenging aspects of creating dice art is ensuring that the final piece looks smooth and cohesive, despite the fact that it is made up of individual dice. To achieve this, the artist must carefully consider the placement of each die, as well as the orientation of its faces. They must also take into account the way that light will interact with the piece, as this can greatly affect the overall appearance of the design.
+One of the most challenging aspects of creating dice art is ensuring that the final piece looks smooth and cohesive, despite the fact that it is made up of individual dice. To achieve this, the artist must carefully consider the placement of each die, as well as the orientation of its faces. They must also take into account the way that light will interact with the piece, as this can greatly affect the overall appearance of the design.<br/><br/>
 
-Despite the challenges, creating dice art can be a highly rewarding experience. Not only does it allow artists to express their creativity and showcase their skills, but it also produces unique and eye-catching pieces that are sure to grab attention. Whether you are a seasoned artist or just starting out, giving dice art a try is a fun and exciting way to explore your creativity and push the boundaries of what is possible with this versatile medium.
+Despite the challenges, creating dice art can be a highly rewarding experience. Not only does it allow artists to express their creativity and showcase their skills, but it also produces unique and eye-catching pieces that are sure to grab attention. Whether you are a seasoned artist or just starting out, giving dice art a try is a fun and exciting way to explore your creativity and push the boundaries of what is possible with this versatile medium.<br/><br/>
         </p>
     </Row>
   )
